4d5e9353734155da9d130cb910ae60fbb4f52395
festus wanjohi <strategic.policyafrica@gmail.com>
2022-08-17 07:05:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+function wrapAdjective(char = "*") {
+  return function (emphatic) {
+    return `You are ${char}${emphatic}${char}!`;
+  };
+}
